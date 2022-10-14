c224962ae75d724c98884e74bbdd12f61eeb31e9
zikaleste09 <115758432+zikaleste09@users.noreply.github.com>
2022-10-13 21:42:22 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function SwitchOn(){
+    document.getElementById("tue").src="https://i.pinimg.com/originals/b0/13/79/b013790a6625dcbd8da4ca662530720d.jpg";
+   }
+   
+   function SwitchOff(){ 
+    document.getElementById("tue2").src="https://i.pinimg.com/originals/b0/13/79/b013790a6625dcbd8da4ca662530720d.jpg";
+   }
